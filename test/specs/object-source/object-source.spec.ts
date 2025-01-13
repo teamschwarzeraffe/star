c9e1a657b77d4614ec086f05dc203351e2c64ee7
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-01-13 20:20:53 +0000

    Remove styles - improving security

--- test/specs/object-source/object-source.spec.ts
+++ test/specs/object-source/object-source.spec.ts
@@ -11,16 +11,16 @@ import bundledSchema from "./bundled.js";
 import { expect } from "vitest";
 
 // @ts-expect-error TS(2345): Argument of type 'string | undefined' is not assig... Remove this comment to see the full error message
-let isWindows = /^win/.test(globalThis.process ? globalThis.process.platform : undefined);
+const isWindows = /^win/.test(globalThis.process ? globalThis.process.platform : undefined);
 
 describe("Object sources (instead of file paths)", () => {
   it("should dereference a single object", async () => {
-    let parser = new $RefParser();
-    let schema = await parser.dereference(helper.cloneDeep(internalRefsParsedSchema));
+    const parser = new $RefParser();
+    const schema = await parser.dereference(helper.cloneDeep(internalRefsParsedSchema));
     expect(schema).to.equal(parser.schema);
     expect(schema).to.deep.equal(internalRefsDereferencedSchema);
     // The schema path should be the current directory
-    let expectedPaths = [path.cwd()];
+    const expectedPaths = [path.cwd()];
     if (!isWindows) {
       expect(parser.$refs.paths()).to.have.same.members(expectedPaths);
       expect(parser.$refs.values()).to.have.keys(expectedPaths);
@@ -41,12 +41,12 @@ describe("Object sources (instead of file paths)", () => {
   });
 
   it("should dereference an object that references external files", async () => {
-    let parser = new $RefParser();
-    let schema = await parser.dereference(helper.cloneDeep(parsedSchema.schema));
+    const parser = new $RefParser();
+    const schema = await parser.dereference(helper.cloneDeep(parsedSchema.schema));
     expect(schema).to.equal(parser.schema);
     expect(schema).to.deep.equal(dereferencedSchema);
     // The schema path should be the current directory, and all other paths should be absolute
-    let expectedPaths = [
+    const expectedPaths = [
       path.cwd(),
       path.abs("test/specs/object-source/definitions/definitions.json"),
       path.abs("test/specs/object-source/definitions/name.yaml"),
@@ -74,12 +74,12 @@ describe("Object sources (instead of file paths)", () => {
   });
 
   it("should bundle an object that references external files", async () => {
-    let parser = new $RefParser();
-    let schema = await parser.bundle(helper.cloneDeep(parsedSchema.schema));
+    const parser = new $RefParser();
+    const schema = await parser.bundle(helper.cloneDeep(parsedSchema.schema));
     expect(schema).to.equal(parser.schema);
     expect(schema).to.deep.equal(bundledSchema);
     // The schema path should be the current directory, and all other paths should be absolute
-    let expectedPaths = [
+    const expectedPaths = [
       path.cwd(),
       path.abs("test/specs/object-source/definitions/definitions.json"),
       path.abs("test/specs/object-source/definitions/name.yaml"),

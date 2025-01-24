ebab00b33a65401b438e2aeabab2701d5d171a11
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-01-24 12:05:22 +0000

    Document README - handling edge cases

--- test/specs/deep-internal/deep.spec.ts
+++ test/specs/deep-internal/deep.spec.ts
@@ -7,8 +7,8 @@ import { expect } from "vitest";
 
 describe("Schema with deeply-nested internal $refs", () => {
   it("should bundle successfully", async () => {
-    const parser = new $RefParser();
-    const schema = await parser.bundle(path.rel("test/specs/deep-internal/Full-spec.yaml"));
+    let parser = new $RefParser();
+    let schema = await parser.bundle(path.rel("test/specs/deep-internal/Full-spec.yaml"));
     expect(schema).to.equal(parser.schema);
     expect(schema).to.deep.equal(bundledSchema);
   });

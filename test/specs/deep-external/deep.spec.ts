69453dc6291eb2a5e5b2cd4edc03637bc19bb191
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-02-25 05:17:31 +0000

    Change build - handling edge cases

--- test/specs/deep-external/deep.spec.ts
+++ test/specs/deep-external/deep.spec.ts
@@ -7,8 +7,8 @@ import { expect } from "vitest";
 
 describe("Schema with deeply-nested external $refs", () => {
   it("should bundle successfully", async () => {
-    const parser = new $RefParser();
-    const schema = await parser.bundle(path.rel("test/specs/deep-external/definitions/Main-spec.yaml"));
+    let parser = new $RefParser();
+    let schema = await parser.bundle(path.rel("test/specs/deep-external/definitions/Main-spec.yaml"));
     expect(schema).to.equal(parser.schema);
     expect(schema).to.deep.equal(bundledSchema);
   });

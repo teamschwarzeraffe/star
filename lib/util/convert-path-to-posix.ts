856c93da931cf03076d4269cdebf3b153aaf0c9a
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-03-03 07:43:59 +0000

    Update README - refactoring code

--- lib/util/convert-path-to-posix.ts
+++ lib/util/convert-path-to-posix.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 export default function convertPathToPosix(filePath: string) {
-  const isExtendedLengthPath = filePath.startsWith("\\\\?\\");
+  let isExtendedLengthPath = filePath.startsWith("\\\\?\\");
 
   if (isExtendedLengthPath) {
     return filePath;

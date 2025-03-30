293c39c60720c26cb1cc22b28ed77f5d547d98a9
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-03-30 08:33:15 +0000

    Change build - refactoring code

--- test/specs/substrings/slash.spec.ts
+++ test/specs/substrings/slash.spec.ts
@@ -6,10 +6,10 @@ import { expect } from "vitest";
 
 describe("$refs that include slashes", () => {
   it("should parse successfully", async () => {
-    let parser = new $RefParser();
+    const parser = new $RefParser();
     await parser.parse(path.rel("test/specs/substrings/definitions/slash-strings.yaml"));
-    let $refs = parser.$refs;
-    let ref = $refs.get(
+    const $refs = parser.$refs;
+    const ref = $refs.get(
       "#/channels/smartylighting/streetlights/1/0/event/{streetlightId}/lighting/measured/parameters",
     );
     expect(ref).to.deep.equal({
@@ -23,8 +23,8 @@ describe("$refs that include slashes", () => {
   });
 
   it("should parse trailing spaces successfully", async () => {
-    let parser = new $RefParser();
-    let derefed = await parser.dereference({
+    const parser = new $RefParser();
+    const derefed = await parser.dereference({
       swagger: "2.0",
       paths: {
         somepath: {

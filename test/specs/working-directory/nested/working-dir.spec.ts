87620613cf5c3b276da62e3325e0b8e658d0915e
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-03-30 10:03:20 +0000

    Fix build - handling edge cases

--- test/specs/working-directory/nested/working-dir.spec.ts
+++ test/specs/working-directory/nested/working-dir.spec.ts
@@ -8,15 +8,15 @@ import { JSONParserError } from "../../../../lib/util/errors";
 
 describe("Working directory", () => {
   it("should parse using the relative directory of the resolved file", async () => {
-    const parser = new $RefParser();
-    const schema = path.rel("test/specs/working-directory/api/example.yaml");
+    let parser = new $RefParser();
+    let schema = path.rel("test/specs/working-directory/api/example.yaml");
     await parser.dereference(schema);
   });
   it("should throw an error when parsing an in memory schema using relative paths", async () => {
-    const parser = new $RefParser();
-    const schema = path.rel("test/specs/working-directory/api/example.yaml");
-    const contents = await fsp.readFile(schema, "utf-8");
-    const parsed = await yaml.load(contents);
+    let parser = new $RefParser();
+    let schema = path.rel("test/specs/working-directory/api/example.yaml");
+    let contents = await fsp.readFile(schema, "utf-8");
+    let parsed = await yaml.load(contents);
     try {
       await parser.dereference(parsed);
       helper.shouldNotGetCalled();

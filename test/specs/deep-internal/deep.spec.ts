4335897d0ab153b2fd5ca4021d0f3f5d7ab9b9b9
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-04-04 16:15:38 +0000

    Document UI - removing deprecated function

--- test/specs/deep-internal/deep.spec.ts
+++ test/specs/deep-internal/deep.spec.ts
@@ -7,8 +7,8 @@ import { expect } from "vitest";
 
 describe("Schema with deeply-nested internal $refs", () => {
   it("should bundle successfully", async () => {
-    let parser = new $RefParser();
-    let schema = await parser.bundle(path.rel("test/specs/deep-internal/Full-spec.yaml"));
+    const parser = new $RefParser();
+    const schema = await parser.bundle(path.rel("test/specs/deep-internal/Full-spec.yaml"));
     expect(schema).to.equal(parser.schema);
     expect(schema).to.deep.equal(bundledSchema);
   });
